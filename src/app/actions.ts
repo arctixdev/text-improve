a9d31cf8657fca9965a2befc623bac3f2bfdac4d
Jonathan Bangert <jonathan@bangert.dk>
2024-05-15 08:59:18 +0200

    Update actions.ts

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -7,7 +7,7 @@ const system_prompt = 'Improve the following text';
 
 export async function getAnswer(question: string) {
   const { text, finishReason, usage } = await generateText({
-    model: openai('gpt-3.5-turbo'),
+    model: openai('gpt-4o'),
     maxTokens: 512,
     system: system_prompt,
     prompt: question,
