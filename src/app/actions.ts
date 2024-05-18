cd7dd1c03c81d1c7560808d5e16afbde93528e93
Jonathan Bangert <jonathan@bangert.dk>
2024-05-18 13:33:59 +0200

    Add copy button

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,7 +3,7 @@
 import { generateText } from 'ai';
 import { openai } from '@ai-sdk/openai';
 
-const system_prompt = 'Improve the following text';
+const system_prompt = 'Rewrite the following text to make it extremely offensive.';
 
 export async function getAnswer(question: string) {
   const { text, finishReason, usage } = await generateText({
@@ -15,3 +15,4 @@ export async function getAnswer(question: string) {
 
   return { text, finishReason, usage };
 }
+
--- src/components/TextImprover.tsx
+++ src/components/TextImprover.tsx
@@ -3,26 +3,36 @@
 import { getAnswer } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { Copy, CopyCheckIcon } from "lucide-react";
 import { useRef, useState } from "react";
 import ReactDiffViewer, { DiffMethod } from 'react-diff-viewer-continued';
 
 export default function TextImprover() {
     const textInput = useRef<HTMLTextAreaElement>(null);
-    const [generation, setGeneration] = useState<string>('');
+    const [generation, setGeneration] = useState<string | null>('');
     const [loading, setLoading] = useState<boolean>(false);
 
     async function improveText() {
         setLoading(true);
+        setGeneration(null);
         const { text } = await getAnswer(textInput.current?.value || '');
         setGeneration(text);
         setLoading(false);
     }
 
+    function copyGeneration() {
+        if (!generation) return;
+        navigator.clipboard.writeText(generation);
+    }
+
     return (
         <div className="flex flex-col gap-2" >
             <Textarea ref={textInput} id="text" className="h-72" placeholder="Enter text to improve" disabled={loading} />
-            <Button onClick={improveText} disabled={loading} >Improve</Button>
-            { (generation) && <ReactDiffViewer oldValue={textInput.current?.value || ''} newValue={generation} splitView={true} compareMethod={DiffMethod.WORDS} /> }
+            <div>
+                <Button onClick={improveText} disabled={loading} >Improve</Button>
+                { generation && <Button onClick={copyGeneration}><Copy/></Button> }
+            </div>
+            { generation && <ReactDiffViewer oldValue={textInput.current?.value || ''} newValue={generation} splitView={true} compareMethod={DiffMethod.WORDS} /> }
         </div>
     );
 }
